f4a950359ae2cf85a2713f8f1670edc6499873d3
lmiguelez <112289271+lmiguelez@users.noreply.github.com>
2022-09-21 16:22:44 -0400

    Flix
    Flix units 1-2

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by E-Z Cool  on 9/20/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+   
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
